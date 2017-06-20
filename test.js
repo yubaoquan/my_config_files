0563cc473d3328b21dced71a009d9777427b58af
yubaoquan <hzyubaoquan@corp.netease.com>
2017-06-20 21:11:21 +0800

    update

---
+++ test.js
@@ -0,0 +1,26 @@
+// This file is used 4 test the .eslintrc file.
+// ++++
+/* = */
+function a() {
+
+    for (let i = 0; i < 10; i++) {
+        console.info(i);
+    }
+    const obj = { a: 1};
+    console.info(obj);
+    // xxxx
+    const str =  'abcdefggggggg99999999999999999999999999999999999999999999gggggggggggggggggggggggggggggggggggggggggg';
+    const str2 =    `abcdefggggggg999999asdfadf99999999999999999999999999999999999999gggggggggggggggggggggggggggggggggggggggggg`;
+    console.info(str, str2);
+
+    const aa =  1 +   2;
+    console.info(aa);
+}
+a();
+[].forEach((item) => {
+    console.info(item);
+});
+
+[].forEach(function(item) {
+    console.info(item);
+});
